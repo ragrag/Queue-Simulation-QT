10bc3ee10ff55a6ad584194fab0f2e9a237b0eea
ragrag <raggi144711@bue.edu.eg>
2018-11-13 17:47:14 +0200

    charts

--- chartwindow.cpp
+++ chartwindow.cpp
@@ -14,28 +14,160 @@ ChartWindow::ChartWindow(QWidget *parent,vector <pair < System,Result> > runList
 
 ChartWindow::~ChartWindow()
 {
-  cout<<"DE";
+   delete ui;
 }
 
 
 
+void ChartWindow::on_svcDPieBtn_clicked()
+{
 
 
+    map <float,int> mp;
+    for (auto v : runList)
+    {
+        mp[ truncf(v.second.avgSvcDrivein * 10) / 10]++;
+    }
+    createPieChart(mp,"Avarage Service Drive-in");
 
+}
 
-void ChartWindow::on_pushButton_18_clicked()
+void ChartWindow::on_svcIPieBtn_clicked()
+{
+    map <float,int> mp;
+    for (auto v : runList)
+    {
+        mp[ truncf(v.second.avgSvcInside * 10) / 10]++;
+    }
+    createPieChart(mp,"Avarage Service Inside");
+}
+
+void ChartWindow::on_waitDPieBtn_clicked()
+{
+    map <float,int> mp;
+    for (auto v : runList)
+    {
+        mp[ truncf(v.second.avgWaitingDrivein * 10) / 10]++;
+    }
+    createPieChart(mp,"Avarage Waiting Drive-in");
+}
+
+void ChartWindow::on_waitIPieBtn_clicked()
+{
+    map <float,int> mp;
+    for (auto v : runList)
+    {
+        mp[ truncf(v.second.avgWaitingInside * 10) / 10]++;
+    }
+    createPieChart(mp,"Avarage Waiting Inside");
+}
+
+void ChartWindow::on_qlPieBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[ v.second.maxQueueLength]++;
+    }
+    createPieChart(mp,"Maximum Queue Length Inside");
+}
+
+void ChartWindow::on_probPieBtn_clicked()
 {
     map <int,int> mp;
     for (auto v : runList)
     {
         mp[v.second.probInside]++;
     }
-    createPieChart(mp);
+    createPieChart(mp,"Probability To Go Inside");
+}
+
+void ChartWindow::on_idlePieBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.idleTime]++;
+    }
+    createPieChart(mp,"Idle Portion Inside");
 }
 
 
 
-void ChartWindow::createPieChart(map<int,int> mp)
+void ChartWindow::on_svcDBarBtn_clicked()
+{
+
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.avgSvcDrivein]++;
+    }
+    createBarChart(mp,"Avarage Service Drive-in");
+}
+
+void ChartWindow::on_svcIBarBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.avgSvcInside]++;
+    }
+    createBarChart(mp,"Avarage Service Inside");
+}
+
+void ChartWindow::on_waitDBarBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.avgWaitingDrivein]++;
+    }
+    createBarChart(mp,"Avarage Waiting Drive-in");
+}
+
+void ChartWindow::on_waitIBarBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.avgWaitingInside]++;
+    }
+    createBarChart(mp,"Avarage Waiting Inside");
+}
+
+void ChartWindow::on_qlBarBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.maxQueueLength]++;
+    }
+    createBarChart(mp,"Maximum Queue Length Inside");
+}
+
+void ChartWindow::on_probBarBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.probInside]++;
+    }
+    createBarChart(mp,"Probability To Go Inside");
+}
+
+void ChartWindow::on_idleBarBtn_clicked()
+{
+    map <int,int> mp;
+    for (auto v : runList)
+    {
+        mp[v.second.idleTime]++;
+    }
+    createBarChart(mp,"Idle Portion Inside");
+}
+
+
+
+   template<typename T> void ChartWindow::createPieChart(T mp,string title)
 {
 
     QPieSeries *series = new QPieSeries();
@@ -45,8 +177,6 @@ void ChartWindow::createPieChart(map<int,int> mp)
     int it =0;
     for(auto v:mp)
     {
-
-        cout<<v.first << " "<<v.second<<endl;
           series->append(QString::number(v.first), v.second);
         if(v.second > maxN)
         {
@@ -66,7 +196,7 @@ void ChartWindow::createPieChart(map<int,int> mp)
 
     QChart *chart = new QChart();
     chart->addSeries(series);
-    chart->setTitle("Simple piechart example");
+    chart->setTitle(QString::fromStdString(title));
     //chart->legend()->hide();
 
     QChartView *chartView = new QChartView(chart);
@@ -85,37 +215,47 @@ void ChartWindow::createPieChart(map<int,int> mp)
 }
 
 
-void ChartWindow::on_svcDPieBtn_clicked()
+
+template<typename T> void ChartWindow::createBarChart(T mp,string title)
 {
 
+
+    QBarSet *set0 = new QBarSet(QString::fromStdString(title));
+    QStringList categories;
+
+
+for (auto v:mp)
+{
+*set0<<(int)v.second;
+ categories<<QString::number(v.first);
 }
+QBarSeries *series = new QBarSeries();
+   series->append(set0);
 
-void ChartWindow::on_svcIPieBtn_clicked()
-{
+   QChart *chart = new QChart();
+   chart->addSeries(series);
+   chart->setTitle(QString::fromStdString(title));
+   chart->setAnimationOptions(QChart::SeriesAnimations);
 
-}
 
-void ChartWindow::on_waitDPieBtn_clicked()
-{
+   QBarCategoryAxis *axis = new QBarCategoryAxis();
+   axis->append(categories);
+   chart->createDefaultAxes();
+   chart->setAxisX(axis, series);
+   chart->legend()->setVisible(true);
+   chart->legend()->setAlignment(Qt::AlignBottom);
+   QChartView *chartView = new QChartView(chart);
+   chartView->setRenderHint(QPainter::Antialiasing);
 
-}
 
-void ChartWindow::on_waitIPieBtn_clicked()
-{
+     QWidget * chartWindow = new QWidget(0);
+     QVBoxLayout *layout = new QVBoxLayout(chartWindow);
+     layout->addWidget(chartView);
+     setLayout(layout);
+     layout->activate();
 
-}
+     chartWindow->resize(800,400);
+     chartWindow->show();
 
-void ChartWindow::on_qlPieBtn_clicked()
-{
-
-}
-
-void ChartWindow::on_probPieBtn_clicked()
-{
-
-}
-
-void ChartWindow::on_idlePieBtn_clicked()
-{
 
 }
--- chartwindow.h
+++ chartwindow.h
@@ -11,7 +11,11 @@
 #include <QtCharts/QChartView>
 #include <QtCharts/QPieSeries>
 #include <QtCharts/QPieSlice>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarCategoryAxis>
 
+#include <string>
 
 QT_CHARTS_USE_NAMESPACE
 using namespace std;
@@ -24,12 +28,42 @@ class ChartWindow : public QMainWindow
     Q_OBJECT
 
 public:
+
     explicit ChartWindow(QWidget *parent = nullptr, vector < pair<System,Result> > runList = vector <pair <System,Result> > () );
     ~ChartWindow();
-    void createPieChart( map<int,int> );
+    template<typename T>  void createPieChart( T mp,string  );
+    template<typename T> void createBarChart(T mp,string title);
 
 private slots:
-    void on_pushButton_18_clicked();
+
+    void on_svcDPieBtn_clicked();
+
+    void on_svcIPieBtn_clicked();
+
+    void on_waitDPieBtn_clicked();
+
+    void on_waitIPieBtn_clicked();
+
+    void on_qlPieBtn_clicked();
+
+    void on_probPieBtn_clicked();
+
+    void on_idlePieBtn_clicked();
+
+
+    void on_svcDBarBtn_clicked();
+
+    void on_svcIBarBtn_clicked();
+
+    void on_waitDBarBtn_clicked();
+
+    void on_waitIBarBtn_clicked();
+
+    void on_qlBarBtn_clicked();
+
+    void on_probBarBtn_clicked();
+
+    void on_idleBarBtn_clicked();
 
 private:
     Ui::ChartWindow *ui;
